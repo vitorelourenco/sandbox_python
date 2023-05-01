4653868208df4567c37d69039f0043450417dba4
vitorelourenco <vitor.registros@lourencos.net>
2023-05-01 17:44:49 -0300

    Add numbers file

---
+++ numbers/numbers.py
@@ -0,0 +1,7 @@
+# In python when you divide you always get a float
+x = 4/2
+print(x) # 2.0
+
+# You can use underscores in numbers to make them more readable
+universe_age = 14_000_000_000
+print(universe_age) # 14000000000
